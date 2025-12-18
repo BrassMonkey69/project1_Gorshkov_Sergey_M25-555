362a11a7399078317243fb72ddc421408035e891
Sergey Gorshkov <Dantistsjohn@mail.ru>
2025-12-18 17:40:25 +0000

    добавил затерявшиеся файлы

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -5,3 +5,10 @@ def main():
 import constants
 import player_actions
 import utils
+
+game_state = {
+        'player_inventory': [], # Инвентарь игрока
+        'current_room': 'entrance', # Текущая комната
+        'game_over': False, # Значения окончания игры
+        'steps_taken': 0 # Количество шагов
+}
