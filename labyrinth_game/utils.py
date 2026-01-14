10fd4325f8ad916d968da9eece06145190868017
Sergey Gorshkov <Dantistsjohn@mail.ru>
2026-01-14 19:43:03 +0000

    добавил условия и  награды за решение разных загадок. перенес pseudo_random, trigger_trap и random_event в начало. Добавил срабатывание trigger_trap в trap_room при неправильном ответе на решение загадки. изменил загадку в library, добавил альтернативные варианты ответов в library , laboratory и hall в constans.py

--- labyrinth_game/utils.py
+++ labyrinth_game/utils.py
@@ -33,97 +33,7 @@ def describe_current_room(game_state):
         print('В этой комнате нет загадок')
     else:
         print("\nКажется, здесь есть загадка (используйте команду solve).")
-
-def solve_puzzle(game_state):
-    """Позволяет игроку решить загадку в текущей комнате."""
-    current_room = game_state['current_room']
-    room_data = ROOMS[current_room]
-    puzzle = room_data['puzzle']
-
-    if not puzzle:
-        print("Загадок здесь нет.")
-        return
-    question, correct_answer = puzzle
-    print(f"\n{question}")
-    user_answer = input("Ваш ответ: ").strip()
-
-    if user_answer == correct_answer:
-        if current_room == 'laboratory': #проверка нахождения в лаборатории
-            print("Правильно! Вы решили загадку, замок шкатулки открылся.")
-            print ("Под кучей бумаг с надписями 'Python - лучший язык программирования' Вы находите блестящий ключ!")
-            game_state['player_inventory'].append('treasure_key') #добавляет пустую пробирку в инвентарь
-                    
-        elif print("Правильно! Вы решили загадку."):
-            room_data['puzzle'] = None  # убираем загадку
-            game_state['steps_taken'] += 1  # награда за решение, надо додумать что-то нормальное
-            print("Вы получили бонус за решение загадки!")
-    else:
-        print("Неверно. Попробуйте снова.")
-       
-
-def attempt_open_treasure(game_state):
-    """Попытка открыть сундук в treasure_room."""
-    current_room = game_state['current_room']
-    inventory = game_state['player_inventory']
-    room_data = ROOMS[current_room]
-    
-    if current_room != 'treasure_room': #проверка на нахождение в комнате treasure_room
-        print("Здесь нет сундука для открытия.")
-        return
-
-    if 'treasure_chest' not in room_data['items']: #проверка наличия сундука в комнате
-        print("Сундук уже открыт!")
-        return
-
-    # Сценарий 1: есть ключ
-    if 'treasure_key' in inventory:
-        print("Вы применяете ключ, и замок щёлкает. Сундук открыт!")
-        room_data['items'].remove('treasure_chest') #убирает treasure_chest из комнаты
-        print("В сундуке сокровище! Вы победили!")
-        game_state['game_over'] = True
-        return
-
-    # Сценарий 2: нет ключа — предлагаем ввести код
-    print("Сундук заперт. Хотите попробовать ввести код? (да/нет)")
-    choice = input("> ").strip().lower()
-    if choice == 'да':
-        current_room = game_state['current_room']
-        room_data = ROOMS[current_room]
-        puzzle = room_data['puzzle']
-
-        if not puzzle:
-            print("Загадок здесь нет.")
-            return
-
-        question, correct_answer = puzzle
-        print(f"\n{question}")
-        user_answer = input("Ваш ответ: ").strip()
-
-        if user_answer == correct_answer:
-            print("Код верен! Замок щёлкает, сундук открывается!")
-            room_data['items'].remove('treasure_chest')
-            print("В сундуке сокровище! Вы победили!")
-            game_state['game_over'] = True
-            return
-        else:
-            print("Неверный код. Сундук остаётся запертым.")
-    if choice == 'нет':
-        print("Вы отступаете от сундука.")
-  
-    
-def show_help():
-    print("\nДоступные команды:")
-    print("  go <direction>  - перейти в направлении (north/south/east/west)")
-    print("  look/l          - осмотреть текущую комнату")
-    print("  take <item>     - поднять предмет")
-    print("  use <item>      - использовать предмет из инвентаря")
-    print("  inventory/i     - показать инвентарь")
-    print("  solve           - попытаться решить загадку в комнате")
-    print("  quit/exit/q     - выйти из игры")
-    print("  help            - показать это сообщение")
-    print("  open            - попытка открыть сундук сокровищ")
-  
-    
+        
 def pseudo_random(seed, modulo):
     """
     Генерирует псевдослучайное целое число в диапазоне [0, modulo) на основе seed.
@@ -238,3 +148,116 @@ def random_event(game_state):
             else:
                 # Если условия не выполнены, просто сообщаем о странном ощущении
                 print("Вам показалось, что пол дрогнул... Но ничего не произошло.")
+
+def solve_puzzle(game_state):
+    """Позволяет игроку решить загадку в текущей комнате."""
+    current_room = game_state['current_room']
+    room_data = ROOMS[current_room]
+    puzzle = room_data['puzzle']
+
+    if not puzzle:
+        print("Загадок здесь нет.")
+        return
+    question, correct_answer, alt_answer = puzzle
+    print(f"\n{question}")
+    user_answer = input("Ваш ответ: ").strip()
+
+    if user_answer == correct_answer or alt_answer:
+        if current_room == 'laboratory': #проверка нахождения в laboratory
+            room_data['puzzle'] = None  # убираем загадку
+            print("Правильно! Вы решили загадку, Вы чувствуете себя без 5 минут профессором, будто извилины начали шевелиться активнее.")
+            print("В награду Вы получаете магистерскую шапочку с еле читаемой надписью 'Yan.....cum'. Не обращая внимания Вы надеваете шапку")
+            game_state['player_inventory'].append('academic_cap') #добавляет academic_cap в инвентарь
+                    
+        elif current_room == 'trap_room': #проверка нахождения в trap_room
+            room_data['puzzle'] = None  # убираем загадку
+            print("Правильно! Вы решили загадку, замок шкатулки открылся.")
+            print("Под кучей бумаг с надписями 'Python - лучший язык программирования' Вы находите блестящий ключ!")
+            game_state['player_inventory'].append('treasure_key') #добавляет treasure_key в инвентарь
+            
+        elif current_room == 'library': #проверка нахождения в library
+            room_data['puzzle'] = None  # убираем загадку
+            print("Правильно! Вы решили загадку, внезапным порывом сквозняка с лежащей на полке книги сдуло слой пыли.")
+            print("Подойдя ближе, Вы обнаруживаете руководство с названием 'Черная магия для чайников'")
+            game_state['player_inventory'].append('black_magic_guide') #добавляет black_magic_guide в инвентарь
+            
+        elif current_room == 'hall': #проверка нахождения в hall
+            room_data['puzzle'] = None  # убираем загадку
+            print("Правильно! Вы решили загадку, Вы слышите щелчок - это открылся сундук.")
+            print("Заглянув внутрь, Вы обнаруживаете золотую монетку")
+            game_state['player_inventory'].append('gold_coin') #добавляет gold_coin в инвентарь       
+            
+            # оставим на случай других комнат                
+        elif print("Правильно! Вы решили загадку."):
+            room_data['puzzle'] = None  # убираем загадку
+            game_state['player_inventory'].append('coin') #добавляет coin в инвентарь    
+            print("Вы получили бонус за решение загадки!")
+    else:
+        if current_room == 'trap_room' : #проверка нахождения в trap_room
+            trigger_trap(game_state)    
+        else:
+            print("Неверно. Попробуйте снова.")
+       
+
+def attempt_open_treasure(game_state):
+    """Попытка открыть сундук в treasure_room."""
+    current_room = game_state['current_room']
+    inventory = game_state['player_inventory']
+    room_data = ROOMS[current_room]
+    
+    if current_room != 'treasure_room': #проверка на нахождение в комнате treasure_room
+        print("Здесь нет сундука для открытия.")
+        return
+
+    if 'treasure_chest' not in room_data['items']: #проверка наличия сундука в комнате
+        print("Сундук уже открыт!")
+        return
+
+    # Сценарий 1: есть ключ
+    if 'treasure_key' in inventory:
+        print("Вы применяете ключ, и замок щёлкает. Сундук открыт!")
+        room_data['items'].remove('treasure_chest') #убирает treasure_chest из комнаты
+        print("В сундуке сокровище! Вы победили!")
+        game_state['game_over'] = True
+        return
+
+    # Сценарий 2: нет ключа — предлагаем ввести код
+    print("Сундук заперт. Хотите попробовать ввести код? (да/нет)")
+    choice = input("> ").strip().lower()
+    if choice == 'да':
+        current_room = game_state['current_room']
+        room_data = ROOMS[current_room]
+        puzzle = room_data['puzzle']
+
+        if not puzzle:
+            print("Загадок здесь нет.")
+            return
+
+        question, correct_answer = puzzle
+        print(f"\n{question}")
+        user_answer = input("Ваш ответ: ").strip()
+
+        if user_answer == correct_answer:
+            print("Код верен! Замок щёлкает, сундук открывается!")
+            room_data['items'].remove('treasure_chest')
+            print("В сундуке сокровище! Вы победили!")
+            game_state['game_over'] = True
+            return
+        else:
+            print("Неверный код. Сундук остаётся запертым.")
+    if choice == 'нет':
+        print("Вы отступаете от сундука.")
+  
+    
+def show_help():
+    print("\nДоступные команды:")
+    print("  go <direction>  - перейти в направлении (north/south/east/west)")
+    print("  look/l          - осмотреть текущую комнату")
+    print("  take <item>     - поднять предмет")
+    print("  use <item>      - использовать предмет из инвентаря")
+    print("  inventory/i     - показать инвентарь")
+    print("  solve           - попытаться решить загадку в комнате")
+    print("  quit/exit/q     - выйти из игры")
+    print("  help            - показать это сообщение")
+    print("  open            - попытка открыть сундук сокровищ")
+  
